b1263b7b3783e31406958fde800b1c1acf6524c6
Vishal Pant <vishalpant.pant65@gmail.com>
2021-03-29 09:34:06 +0530

    Added addNote and removeNote functionality

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk')
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 
 
 //Create add command
@@ -20,8 +20,7 @@ yargs.command({
         }
     },
     handler: function(argv) {
-        console.log('Title:', argv.title)
-        console.log('Body:', argv.body)
+        notes.addNote(argv.title, argv.body)
     }
 })
 
@@ -29,8 +28,15 @@ yargs.command({
 yargs.command({
     command: 'remove',
     describe: 'Remove a note',
-    handler: function() {
-        console.log('Removing note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOptions: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        notes.removeNote(argv.title)
     }
 })
 
--- notes.js
+++ notes.js
@@ -1,5 +1,56 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
 const getNotes = function() {
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = function(title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+    }
+    else {
+        console.log('Note title taken!')
+    }
+}
+
+const removeNote = function(title) {
+    const notes = loadNotes()
+    const updatedNotes = notes.filter(function(note) {
+        return note.title !== title 
+    })
+    updatedNotes.length !== notes.length ? console.log(chalk.green(title, ' removed!')) : console.log(chalk.red('Title not found!'))
+    saveNotes(updatedNotes)
+}
+
+const loadNotes = function() {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    }
+    catch(e) {
+        return []
+    }
+    
+}
+
+const saveNotes = function(notes) {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+module.exports = {
+    'getNotes': getNotes,
+    'addNote': addNote,
+    'removeNote': removeNote
+}
